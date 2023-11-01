b5065a5250f3d1f61ef0cf2b012a3b9ab72e22bb
luna <syjang@agilesoda.ai>
2023-11-01 15:55:20 +0900

    NpsPension 프로젝트 생성

---
+++ src/main/java/com/nps/pension/NpsPensionApplication.java
@@ -0,0 +1,13 @@
+package com.nps.pension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpsPensionApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(NpsPensionApplication.class, args);
+  }
+  
+}
