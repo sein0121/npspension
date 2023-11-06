2dfcfd7dcb6b81fd57df64574a4f55d7e50233b9
luna <syjang@agilesoda.ai>
2023-11-06 16:11:40 +0900

    LUNA - NpsPension API 개발 (WebClient config 수정)

--- src/main/java/com/nps/pension/config/WebClientUtil.java
+++ src/main/java/com/nps/pension/config/WebClientUtil.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -19,6 +20,8 @@ public class WebClientUtil {
     Logger.info("##### WebClient GET CALL #####");
     return webClientConfig.webClient().method(HttpMethod.GET)
         .uri(url)
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
         .retrieve()     // retrieve() : Body 값만 가져오기, exchange() : ClientResponse 상태값과 헤더 가져오기
         .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new Exception(String.valueOf(HttpStatus.BAD_REQUEST))))
         .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new Exception(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR))))
@@ -30,7 +33,9 @@ public class WebClientUtil {
     Logger.info("##### WebClient POST CALL #####");
     return webClientConfig.webClient().method(HttpMethod.POST)
         .uri(url)
+        .contentType(MediaType.APPLICATION_JSON)
         .bodyValue(request)
+        .accept(MediaType.APPLICATION_JSON)
         .retrieve()
         .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new Exception(String.valueOf(HttpStatus.BAD_REQUEST))))
         .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new Exception(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR))))
@@ -42,7 +47,9 @@ public class WebClientUtil {
     Logger.info("##### WebClient DELETE CALL #####");
     return webClientConfig.webClient().method(HttpMethod.DELETE)
         .uri(url)
+        .contentType(MediaType.APPLICATION_JSON)
         .bodyValue(request)
+        .accept(MediaType.APPLICATION_JSON)
         .retrieve()
         .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new Exception(String.valueOf(HttpStatus.BAD_REQUEST))))
         .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new Exception(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR))))
